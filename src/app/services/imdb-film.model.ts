abda57f0f8d51309e43fde3e590ea39d9738434d
eduardo.saatkamp <eduardosaatkamp@gmail.com>
2024-03-31 05:27:05 -0300

    adicionado gitignore e o modelo de base de dados

---
+++ src/app/services/imdb-film.model.ts
@@ -0,0 +1,9 @@
+export class ImdbFilmModel {
+    items: Object[];
+  
+    constructor(items: Object[]) {
+      this.items = items;
+    }
+  }
+
+//   feito o modelo de base de dados, concluído até consumindo API.
